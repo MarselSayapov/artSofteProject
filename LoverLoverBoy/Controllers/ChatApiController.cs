ad45cd21dc4999eb338fd7b0ff466011160fd752
Sayapov Marsel <122860952+MarselSayapov@users.noreply.github.com>
2024-02-18 20:12:43 +0500

    chat service implementation
    Started prescribing ChatService, described message entity and added summary

---
+++ LoverLoverBoy/Controllers/ChatApiController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoverLoverBoy.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class ChatApiController
+{
+    /// <summary>
+    /// Gets all incoming user's messages by his Id
+    /// </summary>
+    /// <param name="userId">Required user id</param>
+    /// <returns>Enumerable object that contains all incoming messages of this user</returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public async Task<IActionResult> GetAllIncomingUserMessages(uint userId)
+    {
+        throw new NotImplementedException();
+    }
+    /// <summary>
+    /// Gets all outcoming user's messages by his Id
+    /// </summary>
+    /// <param name="userId">Required user id</param>
+    /// <returns>Enumerable object that contains all outcoming messages of this user</returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public async Task<IActionResult> GetAllOutComingUserMessages(uint userId)
+    {
+        throw new NotImplementedException();
+    }
+    /// <summary>
+    /// sends a message to another user
+    /// </summary>
+    /// <param name="message">message with content and sender, recipient id</param>
+    /// <returns>message to another user</returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public async Task<IActionResult> SendMessage(Message message)
+    {
+        throw new NotImplementedException();
+    }
+
+}
